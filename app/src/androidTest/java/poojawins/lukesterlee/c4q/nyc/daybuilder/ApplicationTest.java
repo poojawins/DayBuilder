2b5d0b7a389488e242bc329fded9a8916300bfaf
Luke Lee <samsung3star@gmail.com>
2015-06-21 10:56:25 -0400

    initial commit

---
+++ app/src/androidTest/java/poojawins/lukesterlee/c4q/nyc/daybuilder/ApplicationTest.java
@@ -0,0 +1,13 @@
+package poojawins.lukesterlee.c4q.nyc.daybuilder;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
